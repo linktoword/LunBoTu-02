48cd06662b48557091291502d7cfafa4c8f111a4
linktoword <15239420304@163.com>
2018-04-13 16:44:28 +0800

    按钮点击转换图片
    1.格式有误 调整全部格式
    2.用for循环给每个按钮添加点击事件
    3.定义点击事件函数 获取所点击按钮位置
    按钮中有自定义的属性 不可用“ . ”方法获取值  用getAttribute获取自定义的属性值
    判断所点击的位置和按钮当前位置 如果相等 则什么都不做  如果不等 执行计算偏移量
    运行偏移函数
    按钮转换函数

--- 轮播002.js
+++ 轮播002.js
@@ -1,58 +1,77 @@
 window.onload = function(){
-var container = document.getElementById("container");
-var pic = document.getElementById("pic");
-var buttons = document.getElementById("buttons").getElementsByTagName("span");
-var next = document.getElementById("next");
-var prve = document.getElementById("prve");
-var index = 1;
+    var container = document.getElementById("container");
+    var pic = document.getElementById("pic");
+    var buttons = document.getElementById("buttons").getElementsByTagName("span");
+    var next = document.getElementById("next");
+    var prve = document.getElementById("prve");
+    var index = 1;
 
 
-function offset(distances){
-    newStyle = parseInt(pic.style.left) + distances;
-    pic.style.left = newStyle +"px";
-}
+    function offset(distances){
+        newStyle = parseInt(pic.style.left) + distances;
+        pic.style.left = newStyle +"px";
+    }
 
 
-function showButtons() {
-    for( var i = 0; i < buttons.length; i++) {
-        if (buttons[i].className == "selected") {
-            buttons[i].className = "";
-            break;
+    function showButtons() {
+        for( var i = 0; i < buttons.length; i++) {
+            if (buttons[i].className == "selected") {
+                buttons[i].className = "";
+                break;
+            }
+        }
+        buttons[index-1].className = "selected";
+
+    }
+
+    next.onclick = function () { 
+        offset(-520);
+        if (index == 6) {
+            index = 1;
+        }
+        else{
+            index++;
+        }
+        
+        showButtons();  
+        if (parseInt(pic.style.left)  < -3120) {
+            pic.style.left = "-520px";
+        }
+        
+    };
+
+    prve.onclick = function () {
+        offset(520);
+        if (index == 1) {
+            index = 6;
+        }
+        else{
+            index--;
+        }
+        showButtons();
+        if (parseInt(pic.style.left)  > -520) {
+            pic.style.left = "-3120px";
+        }
+        
+    }
+
+    for (var j = 0; j < buttons.length; j++ ){
+        buttons[j].onclick = function() {
+            var newIndex = parseInt(this.getAttribute("index"));
+            if (newIndex == index) {
+                return;
+            }
+            else{
+                var clickOffSet = -520 * (newIndex - index);
+                offset(clickOffSet);
+                index = newIndex;
+                showButtons();
+             
+            }
+           
+
         }
     }
-    buttons[index-1].className = "selected";
-
-}
-
-next.onclick = function () { 
-    offset(-520);
-    if (index == 6) {
-        index = 1;
-    }
-    else{
-        index++;
-    }
-    
-    showButtons();  
-    if (parseInt(pic.style.left)  < -3120) {
-        pic.style.left = "-520px";
-    }
-    
-};
- prve.onclick = function () {
-    offset(520);
-    if (index == 1) {
-        index = 6;
-    }
-    else{
-        index--;
-    }
-    showButtons();
-    if (parseInt(pic.style.left)  > -520) {
-        pic.style.left = "-3120px";
-    }
-     
- }
 
 
 
